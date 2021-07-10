4a53df94e850c05e051f84fbb57012c3aae96eac
Ann Wilson <anwi1017@gmail.com>
2021-07-10 06:37:46 +0000

    TDD and unit testing

---
+++ src/tdd/PrimeNumTest.java
@@ -0,0 +1,20 @@
+package tdd;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeNumTest {
+	
+	@Test
+	public void PositiveTest()
+	{
+		Assert.assertTrue(PrimeNumberCheck.isPrime(13));
+	}
+	
+	@Test
+	public void NegativeTest()
+	{
+		Assert.assertFalse(PrimeNumberCheck.isPrime(10));
+	}
+
+}
